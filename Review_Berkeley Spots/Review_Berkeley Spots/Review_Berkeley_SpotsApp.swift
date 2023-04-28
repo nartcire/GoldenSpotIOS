e2c9fa3f6e602b00455fd2b096bce87bf3585f76
KC Kim <90102947+kindacoolkim@users.noreply.github.com>
2023-04-27 17:23:09 -0700

    Add files via upload

---
+++ Review_Berkeley Spots/Review_Berkeley Spots/Review_Berkeley_SpotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Review_Berkeley_SpotsApp.swift
+//  Review_Berkeley Spots
+//
+//  Created by Kyoungcheol Kim on 4/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Review_Berkeley_SpotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
