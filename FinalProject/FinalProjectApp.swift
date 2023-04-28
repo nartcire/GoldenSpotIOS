b339bdb5b8cfa005993fa655bf3cdca68e56b86f
Christine Fang <94412538+crumbs314@users.noreply.github.com>
2023-04-27 22:18:23 -0700

    Add files via upload

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Michael Wu on 4/24/23.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SwiftUIView()
+        }
+    }
+}
