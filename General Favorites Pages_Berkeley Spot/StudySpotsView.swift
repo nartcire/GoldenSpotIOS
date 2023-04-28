8186bc88cdb58455258c5f9df7f7271a6f6d2d00
KC Kim <90102947+kindacoolkim@users.noreply.github.com>
2023-04-27 18:09:39 -0700

    Add files via upload

---
+++ General Favorites Pages_Berkeley Spot/StudySpotsView.swift
@@ -0,0 +1,9 @@
+//
+//  StudySpotsView.swift
+//  cubstart final project
+//
+//  Created by Claire Meaney on 4/26/23.
+//
+
+import Foundation
+
---
+++ General Favorites Pages_Berkeley Spot/cubstart_final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  cubstart_final_projectApp.swift
+//  cubstart final project
+//
+//  Created by Claire Meaney on 4/24/23.
+//
+
+import SwiftUI
+
+@main
+struct cubstart_final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
