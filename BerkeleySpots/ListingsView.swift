72b2600857d363fe1d9e28eb3b05e22ea2b9a7ec
Eric Tran <erictran1547@gmail.com>
2023-05-01 13:16:53 -0700

    Finished working in the general page

--- BerkeleySpots/ListingsView.swift
+++ BerkeleySpots/ListingsView.swift
@@ -16,9 +16,50 @@ struct ListingsView: View {
     var body: some View {
         NavigationView {
             List {
-                ForEach(firestoreManager.filteredStudySpotList) { temp in
-                    StudySpotView(studySpotInstance: temp)
-                        .padding()
+                ForEach(firestoreManager.filteredStudySpotList) { studySpotInstance in
+                    NavigationLink {
+                        Listing(img: studySpotInstance.img, locationName: studySpotInstance.locationName, locationAddress: studySpotInstance.locationAddress, numStars: studySpotInstance.numStars, wifi: studySpotInstance.wifi, outlets: studySpotInstance.outlets, foodDrink: studySpotInstance.foodDrink, quietSpace: studySpotInstance.quietSpace, onCampus: studySpotInstance.onCampus, restrooms: studySpotInstance.restrooms, openToPublic: studySpotInstance.openToPublic)
+                    } label: {
+                        HStack {
+                            Image(studySpotInstance.img).resizable().frame(width: 130, height: 130).aspectRatio(contentMode: .fill).cornerRadius(10)
+                            
+                            Spacer()
+                            
+                            VStack {
+                                Spacer()
+                                
+                                Text(studySpotInstance.locationName)
+                                    .lineLimit(1)
+                                    .minimumScaleFactor(0.5)
+                                    .padding(10)
+                                    .fontWeight(.heavy)
+                                    .foregroundColor(.white)
+                                
+                                HStack {
+                                    ForEach(1...studySpotInstance.numStars, id: \.self) {star in
+                                        Image(systemName: "star.fill")
+                                    }
+                                    if studySpotInstance.numStars < 5 {
+                                        ForEach(studySpotInstance.numStars + 1...5, id: \.self) {star in
+                                            Image(systemName: "star")
+                                        }
+                                    }
+                                }
+                                .foregroundColor(.yellow)
+                                
+                                Spacer()
+                                
+                                Text("Closes at " + studySpotInstance.closingTime)
+                                    .fontWeight(.medium)
+                                    .foregroundColor(.white)
+                                
+                                Spacer()
+                            }
+                            
+                            Spacer()
+                        }
+                        .frame(width:330, height:130).background(Color("BerkeleyBlue")).cornerRadius(10)
+                    }
                 }
             }
             .navigationTitle("Study Spots")
